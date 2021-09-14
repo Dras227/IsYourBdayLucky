66b666beeaf55fd81bb5adae618e4052e204374c
Manoj Bhat <manudbhat@gmail.com>
2021-09-15 00:36:01 +0530

    completed it with responsiveness

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 //inp-bdate,inp-luckynumber,check
 
-const inpBdate = document.querySelector(".inp-bdate")
-const luckyNo = document.querySelector(".inp-luckynumber")
+const inpBdate = document.getElementById("inp-bdate")
+const luckyNo = document.getElementById("inp-luckynumber")
 const btnCheck = document.getElementById("check")
-
-
+const outDiv = document.getElementById("output")
+outDiv.style.display = "none";
 btnCheck.addEventListener("click",function areYouLucky() {
     var t = inpBdate.value;
-    alert(t[9]);
-    //2021-09-10
-
-
-    if(isNaN(luckyNo)) alert("please enter lucky no")
-
+    outDiv.innerHTML = ""
     
+    //2021-09-10
+    let i = 0;
+    var sum = 0;
+    for(let i=0;i<t.length;i++)
+    {
+        if(t[i]==="-") continue;
+        console.log(sum+=parseInt(t[i]))
+    }
+
+    var msg = ""
+    if(!(inpBdate && inpBdate.value))
+    {
+        msg = "Enter the Your Bday Date"
+    }
+    else if(!(luckyNo && luckyNo.value))
+    {
+        msg = "Enter Your Lucky No."
+    }
+    else if(parseInt(sum) === parseInt(luckyNo.value))
+    {
+        msg = "Your Lucky"
+    }
+    else 
+    {
+        msg = "Your not that lucky....But your damn special"
+    }
+    showMessage(msg);
 })
+
+
+function showMessage(msg) {
+    outDiv.style.display = "block"
+    const node = document.createTextNode(msg)
+
+    const para = document.createElement("p")
+    para.appendChild(node)
+    
+    outDiv.appendChild(para)
+}
+
